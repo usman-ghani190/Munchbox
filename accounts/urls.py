fb5cae1d0e589de10055de9363a3e75238199f73
Usman <Cap.usman21@gmail.com>
2025-05-07 00:02:29 +0500

    working on registration module

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 """Urls for Accounts app"""
 
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -11,4 +11,19 @@ urlpatterns = [
     path('api/register/', views.RegisterAPIView.as_view(), name='api_register'),
     path('api/login/', views.LoginAPIView.as_view(), name='api_login'),
     path('api/logout/', views.LogoutAPIView.as_view(), name='api_logout'),
+    # Password reset URLs
+    path('password/reset/', auth_views.PasswordResetView.as_view(
+        template_name='accounts/password_reset_form.html',
+        email_template_name='accounts/password_reset_email.html',
+        subject_template_name='accounts/password_reset_subject.txt'
+    ), name='password_reset'),
+    path('password/reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='accounts/password_reset_done.html'
+    ), name='password_reset_done'),
+    path('password/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='accounts/password_reset_confirm.html'
+    ), name='password_reset_confirm'),
+    path('password/reset/complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='accounts/password_reset_complete.html'
+    ), name='password_reset_complete'),
 ]
