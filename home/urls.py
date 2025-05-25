2f2161d2ce42068ef61a123f21d12f0495e3cf18
Usman <Cap.usman21@gmail.com>
2025-05-25 22:19:13 +0500

    added tests for user

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,8 @@
 from . import views
 from django.urls import path
 
+app_name = 'home'
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('explore', views.explore, name='explore'),
