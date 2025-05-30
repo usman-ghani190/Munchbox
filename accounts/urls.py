65883972e52ac22ab7c0af617162d474261bbe02
Usman <Cap.usman21@gmail.com>
2025-05-30 21:44:58 +0500

    resolved sign out issue from homepage

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,8 @@ from django.contrib.auth import views as auth_views
 
 from . import views
 
+app_name = 'accounts'
+
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('login', views.login, name='login'),
